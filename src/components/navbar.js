4289089ebc34f23de527c99b4771f05b05271edd
robinseon <robin.seon.rs@gmail.com>
2022-05-19 17:02:06 +0200

    start main

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,7 +10,8 @@ function dropdown(){
 
 const navbar = () => {
     return (
-        <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+      <div class="bg-stone-800">
+        <nav class="bg-stone-600 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-stone-800">
         <div class="container flex flex-wrap justify-between items-center mx-auto">
         <a href="" class="block py-2 pr-4 pl-3 text-white">Launchor</a>
         <div class="flex md:order-2">
@@ -30,11 +31,20 @@ const navbar = () => {
               <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Dashboard</a>
             </li>
             <li>
+              <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">DAO</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Learn</a>
+            </li>
+            <li>
             <button id="dropdownNavbarLink" onClick={dropdown} data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">More <svg class="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
             <div id="dropdownNavbar" class="z-10 hidden origin-top-right absolute bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                 <ul class="py-1 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
                   <li>
                     <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">About the company</a>
+                  </li>
+                  <li>
+                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Token</a>
                   </li>
                   <li>
                     <a href="https://medium.com" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Medium</a>
@@ -55,6 +65,7 @@ const navbar = () => {
         </div>
         </div>
       </nav>
+      </div>  
     );
   };
   
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,33 +2,140 @@ import Footer from '../components/footer';
 import Navbar from '../components/navbar';
 
 const main = () => {
-const ContentPage = () => {
+  const ContentPage = () => {
     return (
-        <div class="m-10"> <h1>Hello World</h1>
-        <div> <h1>Hello World</h1>
-        <h2>Hello world</h2></div>
-        <div> <h1>Hello World</h1></div>
-        <div> <h1>Hello World</h1></div>
-        <div> <h1>Hello World</h1></div>
-        <div> <h1>Hello World</h1></div>
-        <div> <h1>Hello World</h1></div>
-        <div> <h1>Hello World</h1></div>
-        <div> <h1>Hello World</h1></div>
-        <div> <h1>Hello World</h1></div>
-        <div> <h1>Hello World</h1></div>
-        <div> <h1>Hello World</h1></div>
-        <div> <h1>Hello World</h1></div>
-        <div> <h1>Hello World</h1></div></div>
+      <div class="bg-stone-800">
+        <div class="text-white body-font">
+          <div class="grid grid-cols-3 p-10">
+            <h1 class="col-span-2 justify-self-center text-3xl font-sans font-bold p-10">Invest in projects form Start-up</h1>
+            <div class="row-span-2 mx-auto m-auto">Image/Logo here</div>
+            <div class="col-span-2 justify-self-center text-lg text-center px-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+          </div>
+          <div class="border-t border-offset-2 container px-10 py-10 mx-auto">
+            <div class="flex">
+              <h1 class="title-font text-xl font-bold px-10">Current Projects</h1>
+            </div>
+            <div class="flex flex-wrap">
+              <div class="p-10 md:w-1/3">
+                <div class="outline outline-1 rounded-xl overflow-hidden">
+                  <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1618172193622-ae2d025f4032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="blog"></img>
+                  <div class="bg-stone-600 px-6 py-2">
+                    <h2 class=" title-font tracking-widest text-lg title-font font-medium text-gray-400 mb-1 pt-4">Project Title</h2>
+                    <h1 class="title-font text-lg font-medium text-slate-300 mb-3">Project Description</h1>
+                    <p class="flex flex-wrap leading-relaxed text-white h-80 mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolore in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum mollit anim id est laborum anim id.</p>
+                    <div class="grid grid-cols-2 gap-4">
+                      <div class="grid grid-rows-2 gap-4 font-bold">
+                        <div>Aim</div>
+                        <div>Fundraising</div>
+                      </div>
+                      <div class="grid grid-rows-2 justify-end gap-4 ">
+                        <div>20000$</div>
+                        <div>10000$</div>
+                      </div>
+                    </div>
+                    <div class="flex items-center flex-wrap py-4">
+                      <button class="bg-stone-900 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">See Project</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="p-10 md:w-1/3">
+                <div class="rounded-xl overflow-hidden">
+                  <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1624628639856-100bf817fd35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8M2QlMjBpbWFnZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" alt="blog"></img>
+                  <div class="bg-stone-600 px-6 py-2">
+                    <h2 class=" title-font tracking-widest text-lg title-font font-medium text-gray-400 mb-1 pt-4">Project Title</h2>
+                    <h1 class="title-font text-lg font-medium text-slate-300 mb-3">Project Description</h1>
+                    <p class="leading-relaxed text-white h-80 mb-2">Maximum, 500 words.</p>
+                    <div class="grid grid-cols-2 gap-4">
+                      <div class="grid grid-rows-2 gap-4 font-bold">
+                        <div>Aim</div>
+                        <div>Fundraising</div>
+                      </div>
+                      <div class="grid grid-rows-2 justify-end gap-4 ">
+                        <div>20000$</div>
+                        <div>10000$</div>
+                      </div>
+                    </div>
+                    <div class="flex items-center flex-wrap py-4">
+                      <button class="bg-stone-900 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">See Project</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="p-10 md:w-1/3">
+                <div class="rounded-xl overflow-hidden">
+                  <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1618172193622-ae2d025f4032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="blog"></img>
+                  <div class="bg-stone-600 px-6 py-2">
+                    <h2 class=" title-font tracking-widest text-lg title-font font-medium text-gray-400 mb-1 pt-4">Project Title</h2>
+                    <h1 class="title-font text-lg font-medium text-slate-300 mb-3">Project Description</h1>
+                    <p class="leading-relaxed text-white h-80 mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolore in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum mollit anim id est laborum anim id.</p>
+                    <div class="grid grid-cols-2 gap-4">
+                      <div class="grid grid-rows-2 gap-4 font-bold">
+                        <div>Aim</div>
+                        <div>Fundraising</div>
+                      </div>
+                      <div class="grid grid-rows-2 justify-end gap-4 ">
+                        <div>20000$</div>
+                        <div>10000$</div>
+                      </div>
+                    </div>
+                    <div class="flex items-center flex-wrap py-4">
+                      <button class="bg-stone-900 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">See Project</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="p-10 md:w-1/3">
+                <div class="h-full rounded-xl shadow-cla-blue  overflow-hidden">
+                  <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1624628639856-100bf817fd35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8M2QlMjBpbWFnZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" alt="blog"></img>
+                  <div class="bg-stone-600 p-6">
+                    <h2 class=" title-font tracking-widest text-lg title-font font-medium text-gray-400 mb-1 pt-2">Project Title</h2>
+                    <h1 class="title-font text-lg font-medium text-slate-300 mb-3">Project Description</h1>
+                    <p class="leading-relaxed text-white h-80 mb-2">Maximum, 500 words.</p>
+                    <div class="grid grid-cols-2 gap-4">
+                      <div class="grid grid-rows-2 gap-4 font-bold">
+                        <div>Aim</div>
+                        <div>Fundraising</div>
+                      </div>
+                      <div class="grid grid-rows-2 justify-end gap-4 ">
+                        <div>20000$</div>
+                        <div>Add function here</div>
+                      </div>
+                    </div>
+                    <div class="flex items-center flex-wrap py-4">
+                      <button class="bg-stone-900 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">See Project</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="h-full p-10 md:w-1/3">
+                <div class="h-full rounded-xl shadow-cla-blue  overflow-hidden">
+                  <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1631700611307-37dbcb89ef7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60" alt="blog"></img>
+                  <div class="bg-stone-600 p-6">
+                    <h2 class=" title-font tracking-widest text-lg title-font font-medium text-gray-400 mb-1">Project Title</h2>
+                    <h1 class="title-font text-lg font-medium text-slate-300 mb-3">Project Description</h1>
+                    <p class="leading-relaxed text-white mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <div class="flex items-center flex-wrap ">
+                      <button class="bg-stone-900 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">See Project</button>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   };
-  
+
   return (
     <div class="bg-blue">
       <Navbar />
       <ContentPage />
+      <Footer />
     </div>
   );
 }
 
-  export default main;
-  
+export default main;
